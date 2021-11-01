89bca431727808eb718af429ad8b43779a61a32e
Nedine <nedinerafid@gmail.com>
2021-11-01 09:53:17 +0200

    Initial Commit

---
+++ swiftui try/swiftui_tryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tryApp.swift
+//  swiftui try
+//
+//  Created by Yasmine Rafid on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
